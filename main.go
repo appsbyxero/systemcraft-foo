cf52dea9949c177185ad58dcd899abb9e853a259
appsbyxero <appxero@gmail.com>
2024-08-25 12:23:34 +0530

    Provide starter code

--- main.go
+++ main.go
@@ -1,5 +1,50 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"regexp"
+)
+
+func MatchInFile(filePath string, pattern *regexp.Regexp) bool {
+	var file *os.File
+	var err error
+
+	if filePath == "" {
+		file = os.Stdin
+	} else {
+		file, err = os.Open(filePath)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer file.Close()
+	}
+
+	scanner := bufio.NewScanner(file)
+	matched := false
+	for scanner.Scan() {
+		b := scanner.Bytes()
+		fmt.Println(string(b))
+	}
+
+	return matched
+}
+
+// This passes the first test!
 func main() {
-    // Put your code here!
+	exitCode := 0
+	pattern := os.Args[1]
+	matchPattern, err := regexp.Compile(pattern)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	path := os.Args[2]
+	if !MatchInFile(path, matchPattern) {
+		exitCode = 1
+	}
+
+	os.Exit(exitCode)
 }
